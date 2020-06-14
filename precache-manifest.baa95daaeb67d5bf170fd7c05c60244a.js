e8bbcc5dbcfb9cff81c241f5e7e94e8e832b464f
Marian Barbieru <marianbarbieru@gmail.com>
2020-06-15 01:55:04 +0300

    Updates

---
+++ precache-manifest.baa95daaeb67d5bf170fd7c05c60244a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5a2119c34a81e92cd4f68576b714f1",
+    "url": "/Lights/index.html"
+  },
+  {
+    "revision": "ea085571af730d9e60ae",
+    "url": "/Lights/static/css/main.2cad7a50.chunk.css"
+  },
+  {
+    "revision": "41b593abca2b0e9355a8",
+    "url": "/Lights/static/js/2.f5281609.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lights/static/js/2.f5281609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea085571af730d9e60ae",
+    "url": "/Lights/static/js/main.9c43129c.chunk.js"
+  },
+  {
+    "revision": "0a8d2f56f5d720ca8264",
+    "url": "/Lights/static/js/runtime-main.d2dcf2b3.js"
+  }
+]);
